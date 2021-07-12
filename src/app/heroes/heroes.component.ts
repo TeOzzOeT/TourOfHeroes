a8a2bd22c35d3641bb232679b5f0bd54074bd1be
TeOzzOeT <matteo.destefano.its2020@gmail.com>
2021-07-12 17:27:32 +0200

    message dashboard

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -13,11 +13,6 @@ export class HeroesComponent implements OnInit {
   //heroes = HEROES;
   heroes: Hero[] = [];
   
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
   constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
@@ -25,8 +20,8 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
-
 }
 
